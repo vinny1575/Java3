e2b77ad2a77d69460bdaf29ef92e5572bd9bfe48
Mikel Gonzalez <agcompguru@gmail.com>
2013-06-03 12:02:22 -0400

    added animation

---
+++ TheWebBrowserProject/TheWebBrowser/src/main/java/com/ivinny/thewebbrowser/MainActivity.java
@@ -0,0 +1,110 @@
+package com.ivinny.thewebbrowser;
+
+import android.animation.AnimatorInflater;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.URL;
+
+public class MainActivity extends Activity {
+
+    Context ctx = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ctx = this;
+        setContentView(R.layout.activity_main);
+
+        //get the intent from external app
+        Intent intent = getIntent();
+
+        Uri data = intent.getData();
+        URL url = null;
+
+        try {
+            url = new URL(data.getScheme(), data.getHost(),
+                    data.getPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //set webview to open the url in itself instead of default browser
+        final WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+
+        final EditText et = (EditText)findViewById(R.id.editText);
+
+        //check to see if the url came from intent (external app)
+        if(url != null){
+            et.setText(url.toString());
+            webView.loadUrl(url.toString());
+            flip(webView);
+        }
+
+        Button backBtn = (Button)findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.goBack();
+            }
+        });
+
+        Button fwdBtn = (Button)findViewById(R.id.fwdBtn);
+        fwdBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.goForward();
+            }
+        });
+
+        Button goBtn = (Button)findViewById(R.id.goBtn);
+        goBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = et.getText().toString();
+                if(!url.toLowerCase().contains("http://".toLowerCase())){
+                    url = "http://"+url;
+                }
+
+                et.setText(url);
+                webView.loadUrl(url);
+
+                flip(webView);
+            }
+        });
+
+    }
+
+    //flipping animation
+    private void flip(View target){
+        ObjectAnimator anim = ObjectAnimator.ofFloat(ctx, "rotationY", 0.0f, 360.0f);
+        anim.setTarget(target);
+        anim.setDuration(1000);
+        anim.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
